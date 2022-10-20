47ab1814362dc3e6995dfec7e09915b1cfdc5e31
Vale-riad <valentinariad91@gmail.com>
2022-10-20 16:08:10 +0200

    Aggiunge sconto 20%

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@ const km = Number(prompt("quanti Km vuoi percorrere?"));
 // 2. chiedo al passeggero quanti anni ha
 const age = Number(prompt("quanti anni hai?"));
 // 3. calcolare il prezzo del biglietto in base ai Km percorsi
-let price =(km*0.31);
-console.log(km);
+let price =(0.21 * km);
+console.log(price);
 // 4. applicare uno sconto del 20% se il passeggero è minorenne
+if (age<18) {
+    price = price-(price * 0.2);
+}
+console.log(age,price);
 // 5. applicare uno sconto del 40% se il passeggero è un over 65
 // 6. stampare in pagina il prezzo finale del biglietto con massimo due decimali
