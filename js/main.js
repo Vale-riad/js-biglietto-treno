c48342ed4612d720db9076dcbcfd403a2af868fe
Vale-riad <valentinariad91@gmail.com>
2022-10-20 15:25:59 +0200

    Aggiunge in js l'impostazione del problema

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// 1.chiedo al passeggero quanti Km vuole percorrere
+// 2. chiedo al passeggero quanti anni ha
+// 3. calcolare il prezzo del biglietto in base ai Km percorsi
+// 4. applicare uno sconto del 20% se il passeggero è minorenne
+// 5. applicare uno sconto del 40% se il passeggero è un over 65
+// 6. stampare in pagina il prezzo finale del biglietto con massimo due decimali
